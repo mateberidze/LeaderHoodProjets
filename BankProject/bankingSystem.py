547b65ed7306596b88b97ca8887f264a48410571
Vovaadidas777 <mateberidze464@gmail.com>
2024-06-28 11:25:39 +0400

    uptade commit

--- BankProject/bankingSystem.py
+++ BankProject/bankingSystem.py
@@ -16,4 +16,26 @@ def create_account(accounts):
                 accounts[acc_num][1] += amount
             print("amount is deposit succesfully")
         elif deposit_or_withdraw == 2:
-            ~
+            if accounts[acc_num][1] >= amount:
+                account [acc_num][1] -= amount
+                print("Amount withdraw successfuly completed: ")
+                else: 
+                    print("0 balance")
+            else:
+                print("invalid choice")
+        else:
+            print("account not find")
+
+            bank_info = {
+                '123456789':{'account_holder':'mate' 'balance':1000,00}
+                '87654321':{'account_holder': 'luka' 'balance': 5000,00}
+            }
+
+            def uptade_bank_info(acc_num, new_holder, new_balance):
+                if acc_num in bank_info:
+                    bank_info[acc_num]['account_holder'] = new_holder
+                    bank_info[acc_num]['balance'] = new_balance
+                    print("balance updated succesfully")
+                else:
+                    print("account number not found")
+
