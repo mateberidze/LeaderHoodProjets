18d8ce78f0360b43049dd540df313b1982704eb8
Vovaadidas777 <mateberidze464@gmail.com>
2024-06-30 12:49:12 +0400

    uptade commit

--- Password-Generator/src/script.js
+++ Password-Generator/src/script.js
@@ -1,3 +1,4 @@
+const { default: templateBuilder } = require("@babel/template");
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
@@ -16,7 +17,6 @@ function generatePassword() {
   pass1.textContent = password1;
   pass2.textContent = password2;
 }
-
 
 // Slider
 let slider = document.getElementById("myRange");
@@ -46,6 +46,14 @@ function copyToClipboard(element) {
 
 
  
+tempInput.scrollBy();
+
+tempInput.COMMENT_NODE();
+
+templateBuilder();
+
+
+
  
 
 
